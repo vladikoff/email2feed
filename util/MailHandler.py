366a09791059e0ab211c0b05bea22cf08c04997c
vladikoff <start.bat@gmail.com>
2011-02-01 14:13:20 -0500

    added forwarder support

--- util/MailHandler.py
+++ util/MailHandler.py
@@ -9,7 +9,8 @@ import re
 class MailHandler(InboundMailHandler):
     def receive(self, message):
         logging.info("Message from: " + message.sender + " to: " + message.to)
-              
+        original = message.original
+
         fromEmail = message.sender
         to = message.to   
         blocked = trusted = accountExists = blockMode = False        
@@ -22,14 +23,38 @@ class MailHandler(InboundMailHandler):
 
         fm = re.search("(?<=\<)(.*?)(?=\>)", fromEmail) #serching for gmail formatted emails. ex: 'user <user@example.com>'
         if fm: #gmail format
-            fromEmail = fm.group()            
+            fromEmail = fm.group()
+
+        
             
         existingUsers = UserDetails.gql("WHERE emailName = :1 LIMIT 1",emailName) 
         for existingUser in existingUsers:        
                 accountExists = True
                 blockMode = existingUser.trustedMode
                 accountName = existingUser.accountName
-                logging.info("Account Exists: " + str(accountName))    
+                logging.info("Account Exists: " + str(accountName))
+        logging.info("Forwarder start")
+        if original.has_key('X-Forwarded-To'):
+                logging.info("Forwarder hit")
+                forwardEmail = original.get('X-Forwarded-To')
+                to = forwardEmail
+                emailName, emailDomain = forwardEmail.split("@")
+                logging.info("Forwarder: " + str(forwardEmail) + " :: to " + emailName)
+                existingUsers2 = UserDetails.gql("WHERE emailName = :1 LIMIT 1",str(emailName))
+                for existingUser2 in existingUsers2:
+                        accountExists = True
+                        blockMode = existingUser2.trustedMode
+                        accountName = existingUser2.accountName
+                        logging.info("Account Exists via Forward: " + str(emailName))
+                        fromEmail = emailName
+
+
+
+
+
+
+
+        logging.info("Total: " + str(accountExists))
             
         if accountExists:             
             if blockMode: #if trust only enabled
